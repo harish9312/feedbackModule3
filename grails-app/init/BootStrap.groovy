59312f3bb908c6776c52a0211a99721e89f9cae5
Harish Soni <hksoni.9312@gmail.com>
2016-12-24 12:37:12 +0530

    Role creation done for USER_ROLE in bootstrap.groovy

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,8 @@
 class BootStrap {
 
     def init = { servletContext ->
+        def role =  new feedback_database.Role(authority:'ROLE_USER', enabled:'true')
+        role.save()
 
     }
     def destroy = {
